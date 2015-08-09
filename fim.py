1b3ef327661c4686957ef977e1d9ecd09fcb2086
Christer Edwards <cedwards@adobe.com>
2015-08-09 13:49:24 -0600

    updated fim.py to standardize with argos runner

--- fim.py
+++ fim.py
@@ -6,27 +6,31 @@ from __future__ import absolute_import
 
 import os
 import sys
+import gzip
+import json
 import logging
 import salt.utils
+from time import strftime
+from salt.utils.serializers.msgpack import serialize
 
 HAS_PRELINK = False
 
 PRELINK_PATHS = [
-    '/bin',
-    '/lib',
-    '/sbin',
-    '/lib64',
-    '/usr/bin',
-    '/usr/lib',
-    '/usr/sbin',
-    '/usr/lib64',
-    '/usr/games',
-    '/usr/libexec',
-    '/var/ftp/bin',
-    '/var/ftp/lib'
-    '/var/ftp/lib64'
-    '/usr/kerberos/bin',
-]
+        '/bin',
+        '/lib',
+        '/sbin',
+        '/lib64',
+        '/usr/bin',
+        '/usr/lib',
+        '/usr/sbin',
+        '/usr/lib64',
+        '/usr/games',
+        '/usr/libexec',
+        '/var/ftp/bin',
+        '/var/ftp/lib'
+        '/var/ftp/lib64'
+        '/usr/kerberos/bin',
+        ]
 
 LOG = logging.getLogger(__name__)
 
@@ -81,41 +85,81 @@ def _stats(target):
     return __salt__['file.stats'](target)
 
 
-def checksum(algo='sha256', targets=[], *args, **kwargs):
+def _collection(checksums, algo, path):
+    '''
+    standardizing the dictionary creation
+    '''
+    stats = _stats(path)
+    digest = _hasher(algo, path)
+
+    stats.update({'digest': digest, 'algo': algo})
+
+    checksums['files'].append(stats)
+
+    return checksums
+
+
+def _compress(checksums, filename):
+    '''
+    json-ize and write the content
+    '''
+    json_formatted = json.dumps(checksums)
+
+    with gzip.open(filename, 'w') as compressed:
+        compressed.writelines(json_formatted)
+
+    return 'wrote {0}: (gzipped)'.format(filename)
+
+
+def checksum(algo='sha256', targets=[], filename='', *args, **kwargs):
     '''
     Generate dictionary of hashes and corresponding filenames.
 
     Supports file paths and or directories.
     '''
+    checksums = {'files': []}
+    timestamp = strftime("%Y-%m-%d %H:%M:%S")
 
-    checksums = {}
+    ## check for preconfigured filename
+    if not filename:
+        try:
+            if __salt__['config.get']('fim:filename'):
+                filename = __salt__['config.get']('fim:filename')
+        except KeyError:
+            LOG.debug('No filename defined. Sending to stdout')
 
-    if not targets:
+    ## check for preconfigured algos
+    if not algo:
         try:
             if __salt__['config.get']('fim:algo'):
                 algo = __salt__['config.get']('fim:algo')
+        except KeyError:
+            LOG.debug('No algorithm defined. Defaulting to sha256')
+
+    ## check for preconfigured targets
+    if not targets:
+        try:
             if __salt__['config.get']('fim:targets'):
                 targets = __salt__['config.get']('fim:targets')
         except:
-            LOG.debug('No targets found in minion config')
+            return 'No targets defined. Exiting'
 
+    ## iterate through list of targets and generate checksums
     for target in targets:
         if os.path.isdir(target):
             for root, dirs, files in os.walk(target):
                 for file_ in files:
-                    fullpath = os.path.join(root, file_)
-                    if os.path.isfile(fullpath):
-                        stats = _stats(fullpath)
-                        digest = _hasher(algo, fullpath)
-                        checksums.update({fullpath: {}})
-                        checksums.update({fullpath: stats})
-                        checksums[fullpath].update({'checksum': digest, 'algo': algo})
+                    target = os.path.join(root, file_)
+                    if os.path.isfile(target):
+                        checksums = _collection(checksums, algo, target)
         if os.path.isfile(target):
-            stats = _stats(target)
-            digest = _hasher(algo, target)
-            checksums.update({target: {}})
-            checksums.update({target: stats})
-            checksums[target].update({'checksum': digest, 'algo': algo})
+            checksums = _collection(checksums, algo, target)
+
+    checksums['files'].append({'timestamp':timestamp})
+    ## if filename configured, write results to disk
+    if filename:
+        ret = _compress(checksums, filename)
+        return ret
 
     return checksums
 
